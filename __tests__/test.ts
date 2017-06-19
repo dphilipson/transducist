154c47ffffb1dd0f2a5ba003965f1d320dd908bf
David Philipson <david.philipson@gmail.com>
2017-06-19 10:28:53 -0700

    Implement typescript-transducers
    Based on transducers-js, with appropriate typings written. Right now,
    targets es6 and the generated declaration file isn't right, but
    runtime behavior is correct and fully tested. Only thing still needing
    to be implemented is toIter().

--- __tests__/test.ts
+++ __tests__/test.ts
@@ -1,8 +1,264 @@
-import * as t from "../src/index";
+import * as t from "transducers-js";
+import { chainFrom, transducerBuilder } from "../src/index";
 
-describe("Jest", () => {
-    it("should run tests", () => {
-        t.hello();
-        expect(2).toEqual(2);
+describe("toArray()", () => {
+    const input = ["a", "bb", "ccc"];
+
+    it("should return an input array if no transforms", () => {
+        const result = chainFrom(input).toArray();
+        expect(result).toEqual(input);
+    });
+
+    it("should convert iterable input to an array", () => {
+        const set = new ArrayIterator(input);
+        const result = chainFrom(set).toArray();
+        expect(result).toEqual(input);
     });
 });
+
+describe("transformer chain", () => {
+    it("should apply transformations in order", () => {
+        const input = [1, 2, 3, 4, 5];
+        const inc = (n: number) => n + 1;
+        const isEven = (n: number) => n % 2 === 0;
+        const result1 = chainFrom(input).map(inc).filter(isEven).toArray();
+        const result2 = chainFrom(input).filter(isEven).map(inc).toArray();
+        expect(result1).toEqual([2, 4, 6]);
+        expect(result2).toEqual([3, 5]);
+    });
+});
+
+describe("compose()", () => {
+    it("should apply the specified transform", () => {
+        const result = chainFrom(["a", "bb", "ccc"])
+            .compose(t.map((s: string) => s.length))
+            .toArray();
+        expect(result).toEqual([1, 2, 3]);
+    });
+});
+
+describe("map()", () => {
+    it("should map over elements", () => {
+        const result = chainFrom(["a", "bb", "ccc"])
+            .map(s => s.length)
+            .toArray();
+        expect(result).toEqual([1, 2, 3]);
+    });
+});
+
+describe("filter()", () => {
+    it("should remove elements not matching the filter", () => {
+        const result = chainFrom([1, 2, 3, 4, 5])
+            .filter(n => n % 2 === 0)
+            .toArray();
+        expect(result).toEqual([2, 4]);
+    });
+});
+
+describe("remove()", () => {
+    it("should remove elements matching the filter", () => {
+        const result = chainFrom([1, 2, 3, 4, 5])
+            .remove(n => n % 2 === 0)
+            .toArray();
+        expect(result).toEqual([1, 3, 5]);
+    });
+});
+
+describe("keep()", () => {
+    it("should map elements and keep non-nulls", () => {
+        const map: { [key: string]: boolean | null | undefined } = {
+            a: true,
+            b: null,
+            c: true,
+            d: undefined,
+            e: false,
+        };
+        const result = chainFrom(["a", "b", "c", "d", "e"])
+            .keep(s => map[s])
+            .toArray();
+        expect(result).toEqual([true, true, false]);
+    });
+});
+
+describe("mapcat()", () => {
+    it("should map then concatenate elements", () => {
+        const result = chainFrom(["a", "bb", "ccc"])
+            .mapcat(s => s.split(""))
+            .toArray();
+        expect(result).toEqual(["a", "b", "b", "c", "c", "c"]);
+    });
+});
+
+describe("dedupe()", () => {
+    it("should remove consecutive duplicates", () => {
+        const result = chainFrom([1, 2, 2, 3, 3, 3]).dedupe().toArray();
+        expect(result).toEqual([1, 2, 3]);
+    });
+});
+
+describe("take()", () => {
+    it("should take the first n elements", () => {
+        const result = chainFrom([1, 2, 3, 4, 5]).take(3).toArray();
+        expect(result).toEqual([1, 2, 3]);
+    });
+});
+
+describe("takeWhile()", () => {
+    it("should take elements until the predicate fails", () => {
+        const result = chainFrom([1, 2, 3, 4, 5])
+            .takeWhile(n => n < 3)
+            .toArray();
+        expect(result).toEqual([1, 2]);
+    });
+});
+
+describe("takeNth()", () => {
+    it("should take every nth element", () => {
+        const result = chainFrom([1, 2, 3, 4, 5]).takeNth(2).toArray();
+        expect(result).toEqual([1, 3, 5]);
+    });
+});
+
+describe("drop()", () => {
+    it("should drop the first n elements", () => {
+        const result = chainFrom([1, 2, 3, 4, 5]).drop(2).toArray();
+        expect(result).toEqual([3, 4, 5]);
+    });
+});
+
+describe("dropWhile()", () => {
+    it("should drop elements until the predicate fails", () => {
+        const result = chainFrom([1, 2, 3, 4, 5])
+            .dropWhile(n => n < 3)
+            .toArray();
+        expect(result).toEqual([3, 4, 5]);
+    });
+});
+
+describe("partition()", () => {
+    it("should group elements by the specified size", () => {
+        const result = chainFrom([1, 2, 3, 4, 5]).partition(2).toArray();
+        expect(result).toEqual([[1, 2], [3, 4], [5]]);
+    });
+});
+
+describe("partitionBy()", () => {
+    it("should group elements with the same function value", () => {
+        const result = chainFrom(["a", "b", "cc", "dd", "e"])
+            .partitionBy(s => s.length)
+            .toArray();
+        expect(result).toEqual([["a", "b"], ["cc", "dd"], ["e"]]);
+    });
+});
+
+describe("interpose()", () => {
+    it("should insert the separator between elements", () => {
+        const result = chainFrom([1, 2, 3]).interpose(0).toArray();
+        expect(result).toEqual([1, 0, 2, 0, 3]);
+    });
+});
+
+describe("reduce()", () => {
+    const aPush = <T>(array: T[], x: T): T[] => {
+        array.push(x);
+        return array;
+    };
+
+    const transformer: t.Transformer<number[], number> = {
+        ["@@transducer/init"]: () => [],
+        ["@@transducer/result"]: x => x,
+        ["@@transducer/step"]: aPush,
+    };
+
+    it("should use a reducer and initial value", () => {
+        const result = chainFrom([1, 2, 3]).map(n => 2 * n).reduce(aPush, []);
+        expect(result).toEqual([2, 4, 6]);
+    });
+
+    it("should use a transformer and no initial value", () => {
+        const result = chainFrom([1, 2, 3]).map(n => 2 * n).reduce(transformer);
+        expect(result).toEqual([2, 4, 6]);
+    });
+
+    it("should use a transformer and initial value", () => {
+        const result = chainFrom([1, 2, 3])
+            .map(n => 2 * n)
+            .reduce(transformer, [1]);
+        expect(result).toEqual([1, 2, 4, 6]);
+    });
+});
+
+describe("toIterator()", () => {
+    it("should return an iterator of the elements", () => {
+        const iterator = chainFrom([1, 2, 3]).map(n => 2 * n).toIterator();
+        const result: number[] = [];
+        for (
+            let { done, value } = iterator.next();
+            !done;
+            { done, value } = iterator.next()
+        ) {
+            result.push(value);
+        }
+        expect(result).toEqual([2, 4, 6]);
+    });
+});
+
+describe("forEach()", () => {
+    it("should call the provided function on each input", () => {
+        const input = ["a", "bb", "ccc"];
+        const result: number[] = [];
+        chainFrom(input).map(s => s.length).forEach(n => result.push(n));
+        expect(result).toEqual([1, 2, 3]);
+    });
+});
+
+describe("first()", () => {
+    const input = [1, 2, 3, 4, 5];
+
+    it("should return the first element if it exists", () => {
+        const result = chainFrom(input).map(x => 2 * x).drop(2).first();
+        expect(result).toEqual(6);
+    });
+
+    it("should return null if there are no elements", () => {
+        const result = chainFrom(input).filter(n => n > 10).first();
+        expect(result).toBeNull();
+    });
+});
+
+describe("transducer builder", () => {
+    it("should return the identity if no transforms provided", () => {
+        const transducer = transducerBuilder<number>().build();
+        const result = t.into([], transducer, [1, 2, 3]);
+        expect(result).toEqual([1, 2, 3]);
+    });
+
+    it("should return the composition of the specified operations", () => {
+        const transducer = transducerBuilder<number>()
+            .map(x => x + 1)
+            .filter(x => x % 2 === 0)
+            .build();
+        const result = t.into([], transducer, [1, 2, 3]);
+        expect(result).toEqual([2, 4]);
+    });
+});
+
+class ArrayIterator<T> implements IterableIterator<T> {
+    private i: number = 0;
+
+    constructor(private readonly array: T[]) {}
+
+    public [Symbol.iterator]() {
+        return this;
+    }
+
+    public next(): IteratorResult<T> {
+        const { i, array } = this;
+        if (i < array.length) {
+            this.i++;
+            return { done: false, value: array[i] };
+        } else {
+            return { done: true } as any;
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,358 @@
-export function hello() {
-    // tslint:disable-next-line:no-console
-    console.log("Hello, world!");
+import * as t from "transducers-js";
+
+export interface TransformChain<T> {
+    compose<U>(transducer: t.Transducer<T, U>): TransformChain<U>;
+
+    map<U>(f: (item: T) => U): TransformChain<U>;
+    filter(pred: (item: T) => boolean): TransformChain<T>;
+    remove(pred: (item: T) => boolean): TransformChain<T>;
+    keep<U>(f: (item: T) => U | null | void): TransformChain<U>;
+    mapcat<U>(f: (item: T) => U[]): TransformChain<U>;
+    dedupe(): TransformChain<T>;
+    take(n: number): TransformChain<T>;
+    takeWhile(pred: (item: T) => boolean): TransformChain<T>;
+    takeNth(n: number): TransformChain<T>;
+    drop(n: number): TransformChain<T>;
+    dropWhile(pred: (item: T) => boolean): TransformChain<T>;
+    partition(n: number): TransformChain<T[]>;
+    partitionBy(pred: (item: T) => any): TransformChain<T[]>;
+    interpose(separator: T): TransformChain<T>;
+
+    reduce<ResultT>(
+        reducer: t.Reducer<ResultT, T>,
+        initialValue: ResultT,
+    ): ResultT;
+    reduce<ResultT, CompleteResultT>(
+        transformer: t.CompletingTransformer<ResultT, CompleteResultT, T>,
+        initialValue?: ResultT,
+    ): CompleteResultT;
+
+    toArray(): T[];
+    toIterator(): IterableIterator<T>;
+    forEach(f: (item: T) => void): void;
+    first(): T | null;
+}
+
+export interface TransducerBuilder<BaseT, T> {
+    compose<U>(transducer: t.Transducer<T, U>): TransducerBuilder<BaseT, U>;
+
+    map<U>(f: (item: T) => U): TransducerBuilder<BaseT, U>;
+    filter(pred: (item: T) => boolean): TransducerBuilder<BaseT, T>;
+    remove(pred: (item: T) => boolean): TransducerBuilder<BaseT, T>;
+    keep<U>(f: (item: T) => U | null | void): TransducerBuilder<BaseT, U>;
+    mapcat<U>(f: (item: T) => U[]): TransducerBuilder<BaseT, U>;
+    dedupe(): TransducerBuilder<BaseT, T>;
+    take(n: number): TransducerBuilder<BaseT, T>;
+    takeWhile(pred: (item: T) => boolean): TransducerBuilder<BaseT, T>;
+    takeNth(n: number): TransducerBuilder<BaseT, T>;
+    drop(n: number): TransducerBuilder<BaseT, T>;
+    dropWhile(pred: (item: T) => boolean): TransducerBuilder<BaseT, T>;
+    partition(n: number): TransducerBuilder<BaseT, T[]>;
+    partitionBy(pred: (item: T) => boolean): TransducerBuilder<BaseT, T[]>;
+    interpose(separator: T): TransducerBuilder<BaseT, T>;
+
+    build(): t.Transducer<BaseT, T>;
+}
+
+export function chainFrom<T>(collection: T[] | Iterable<T>): TransformChain<T> {
+    return new TransducerChain<T, T>(collection);
+}
+
+export function transducerBuilder<T>(): TransducerBuilder<T, T> {
+    return new TransducerChain<T, T>([]);
+}
+
+type CombinedBuilder<BaseT, T> = TransformChain<T> &
+    TransducerBuilder<BaseT, T>;
+
+class TransducerChain<BaseT, T> implements CombinedBuilder<BaseT, T> {
+    private readonly transducers: Array<t.Transducer<any, any>> = [];
+
+    constructor(private readonly collection: BaseT[] | Iterable<BaseT>) {}
+
+    public compose<U>(
+        transducer: t.Transducer<T, U>,
+    ): CombinedBuilder<BaseT, U> {
+        this.transducers.push(transducer);
+        return this as any;
+    }
+
+    public build(): t.Transducer<BaseT, T> {
+        // Don't use comp() from transducers-js because it fails on 0 or 1
+        // inputs.
+        return (x: any) => {
+            let result = x;
+            for (let i = this.transducers.length - 1; i >= 0; i--) {
+                result = this.transducers[i](result);
+            }
+            return result;
+        };
+    }
+
+    public reduce<ResultT>(
+        reducer: t.Reducer<ResultT, T>,
+        initialValue: ResultT,
+    ): ResultT;
+    public reduce<ResultT, CompleteResultT>(
+        transformer: t.CompletingTransformer<ResultT, CompleteResultT, T>,
+        initialValue?: ResultT,
+    ): CompleteResultT;
+    public reduce<ResultT, CompleteResultT>(
+        transformer:
+            | t.Reducer<ResultT, T>
+            | t.CompletingTransformer<ResultT, CompleteResultT, T>,
+        initialValue: ResultT,
+    ): CompleteResultT {
+        // Need to contort the type system a bit to get this overload.
+        if (typeof transformer === "function") {
+            const result: ResultT = t.transduce<ResultT, BaseT, T>(
+                this.build(),
+                transformer,
+                initialValue,
+                this.collection,
+            );
+            // Safe because ResultT and CompleteResultT are the same in this
+            // case.
+            return result as any;
+        } else {
+            if (initialValue === undefined) {
+                return t.transduce<ResultT, CompleteResultT, BaseT, T>(
+                    this.build(),
+                    transformer,
+                    this.collection,
+                );
+            } else {
+                return t.transduce<ResultT, CompleteResultT, BaseT, T>(
+                    this.build(),
+                    transformer,
+                    initialValue,
+                    this.collection,
+                );
+            }
+        }
+    }
+
+    public toArray(): T[] {
+        return t.into([], this.build(), this.collection);
+    }
+
+    public toIterator(): IterableIterator<T> {
+        return t.toIter(this.collection, this.build());
+    }
+
+    public forEach(f: (item: T) => void): void {
+        this.reduce(new ForEach(f));
+    }
+
+    public first(): T | null {
+        return this.reduce(firstTransformer<T>());
+    }
+
+    public map<U>(f: (item: T) => U): CombinedBuilder<BaseT, U> {
+        return this.compose(t.map(f));
+    }
+
+    public filter(pred: (item: T) => boolean): CombinedBuilder<BaseT, T> {
+        return this.compose(t.filter(pred));
+    }
+
+    public remove(pred: (item: T) => boolean): CombinedBuilder<BaseT, T> {
+        return this.compose(t.remove(pred));
+    }
+
+    public keep<U>(f: (item: T) => U | null | void): CombinedBuilder<BaseT, U> {
+        return this.compose(keep(f));
+    }
+
+    public mapcat<U>(f: (item: T) => U[]): CombinedBuilder<BaseT, U> {
+        return this.compose(t.mapcat(f));
+    }
+
+    public dedupe(): CombinedBuilder<BaseT, T> {
+        return this.compose(dedupe<T>());
+    }
+
+    public take(n: number): CombinedBuilder<BaseT, T> {
+        return this.compose(t.take<T>(n));
+    }
+
+    public takeWhile(pred: (item: T) => boolean): CombinedBuilder<BaseT, T> {
+        return this.compose(t.takeWhile(pred));
+    }
+
+    public takeNth(n: number): CombinedBuilder<BaseT, T> {
+        return this.compose(t.takeNth<T>(n));
+    }
+
+    public drop(n: number): CombinedBuilder<BaseT, T> {
+        return this.compose(t.drop<T>(n));
+    }
+
+    public dropWhile(pred: (item: T) => boolean): CombinedBuilder<BaseT, T> {
+        return this.compose(t.dropWhile(pred));
+    }
+
+    public partition(n: number): CombinedBuilder<BaseT, T[]> {
+        return this.compose(t.partitionAll<T>(n));
+    }
+
+    public partitionBy(pred: (item: T) => any): CombinedBuilder<BaseT, T[]> {
+        return this.compose(t.partitionBy(pred));
+    }
+
+    public interpose(separator: T): CombinedBuilder<BaseT, T> {
+        return this.compose(interpose(separator));
+    }
+}
+
+class Keep<TResult, TCompleteResult, TInput, TOutput>
+    implements t.CompletingTransformer<TResult, TCompleteResult, TInput> {
+    constructor(
+        private readonly f: (x: TInput) => TOutput | null | void,
+        private readonly xf: t.CompletingTransformer<
+            TResult,
+            TCompleteResult,
+            TOutput
+        >,
+    ) {}
+
+    public ["@@transducer/init"](): TResult | void {
+        return this.xf["@@transducer/init"]();
+    }
+
+    public ["@@transducer/result"](result: TResult): TCompleteResult {
+        return this.xf["@@transducer/result"](result);
+    }
+
+    public ["@@transducer/step"](
+        result: TResult,
+        input: TInput,
+    ): TResult | t.Reduced<TResult> {
+        const output = this.f(input);
+        return output == null
+            ? result
+            : this.xf["@@transducer/step"](result, output);
+    }
+}
+
+function keep<T, U>(f: (item: T) => U | null | void): t.Transducer<T, U> {
+    return <TResult, TCompleteResult>(
+        xf: t.CompletingTransformer<TResult, TCompleteResult, U>,
+    ) => new Keep<TResult, TCompleteResult, T, U>(f, xf);
+}
+
+class Dedupe<TResult, TCompleteResult, TInput>
+    implements t.CompletingTransformer<TResult, TCompleteResult, TInput> {
+    private last?: TInput;
+
+    constructor(
+        private readonly xf: t.CompletingTransformer<
+            TResult,
+            TCompleteResult,
+            TInput
+        >,
+    ) {}
+
+    public ["@@transducer/init"](): TResult | void {
+        return this.xf["@@transducer/init"]();
+    }
+
+    public ["@@transducer/result"](result: TResult): TCompleteResult {
+        return this.xf["@@transducer/result"](result);
+    }
+
+    public ["@@transducer/step"](
+        result: TResult,
+        input: TInput,
+    ): TResult | t.Reduced<TResult> {
+        if (input !== this.last) {
+            this.last = input;
+            return this.xf["@@transducer/step"](result, input);
+        } else {
+            return result;
+        }
+    }
+}
+
+function dedupe<T>(): t.Transducer<T, T> {
+    return <TResult, TCompleteResult>(
+        xf: t.CompletingTransformer<TResult, TCompleteResult, T>,
+    ) => new Dedupe(xf);
+}
+
+class Interpose<TResult, TCompleteResult, TInput>
+    implements t.CompletingTransformer<TResult, TCompleteResult, TInput> {
+    private isStarted: boolean = false;
+
+    constructor(
+        private readonly separator: TInput,
+        private readonly xf: t.CompletingTransformer<
+            TResult,
+            TCompleteResult,
+            TInput
+        >,
+    ) {}
+
+    public ["@@transducer/init"](): TResult | void {
+        return this.xf["@@transducer/init"]();
+    }
+
+    public ["@@transducer/result"](result: TResult): TCompleteResult {
+        return this.xf["@@transducer/result"](result);
+    }
+
+    public ["@@transducer/step"](
+        result: TResult,
+        input: TInput,
+    ): TResult | t.Reduced<TResult> {
+        if (this.isStarted) {
+            const withSeparator = this.xf["@@transducer/step"](
+                result,
+                this.separator,
+            );
+            if (t.isReduced(withSeparator)) {
+                return withSeparator;
+            } else {
+                return this.xf["@@transducer/step"](
+                    withSeparator as TResult,
+                    input,
+                );
+            }
+        } else {
+            this.isStarted = true;
+            return this.xf["@@transducer/step"](result, input);
+        }
+    }
+}
+
+function interpose<T>(separator: T): t.Transducer<T, T> {
+    return <TResult, TCompleteResult>(
+        xf: t.CompletingTransformer<TResult, TCompleteResult, T>,
+    ) => new Interpose(separator, xf);
+}
+
+class ForEach<T> implements t.Transformer<void, T> {
+    constructor(private readonly f: (item: T) => void) {}
+
+    public ["@@transducer/init"]() {
+        return undefined;
+    }
+
+    public ["@@transducer/result"]() {
+        return undefined;
+    }
+
+    public ["@@transducer/step"](_: void, input: T) {
+        return this.f(input);
+    }
+}
+
+const firstTransformerConstant: t.Transformer<any, any> = {
+    ["@@transducer/init"]: () => null,
+    ["@@transducer/result"]: (result: any) => result,
+    ["@@transducer/step"]: (_: void, input: any) => t.reduced(input),
+};
+
+function firstTransformer<T>(): t.Transformer<T | null, T> {
+    return firstTransformerConstant;
 }
