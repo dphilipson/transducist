b605d78bef2d7d8cfb5993ea95c6df1b635443fb
David Philipson <david.philipson@gmail.com>
2017-06-22 20:08:30 -0700

    Naive implementation of .toIterator()

--- src/index.ts
+++ src/index.ts
@@ -18,14 +18,14 @@ export interface TransformChain<T> {
     partitionBy(pred: (item: T) => any): TransformChain<T[]>;
     interpose(separator: T): TransformChain<T>;
 
-    reduce<ResultT>(
-        reducer: t.Reducer<ResultT, T>,
-        initialValue: ResultT,
-    ): ResultT;
-    reduce<ResultT, CompleteResultT>(
-        transformer: t.CompletingTransformer<ResultT, CompleteResultT, T>,
-        initialValue?: ResultT,
-    ): CompleteResultT;
+    reduce<TResult>(
+        reducer: t.Reducer<TResult, T>,
+        initialValue: TResult,
+    ): TResult;
+    reduce<TResult, TCompleteResult>(
+        transformer: t.CompletingTransformer<TResult, TCompleteResult, T>,
+        initialValue?: TResult,
+    ): TCompleteResult;
 
     toArray(): T[];
     toIterator(): IterableIterator<T>;
@@ -33,28 +33,28 @@ export interface TransformChain<T> {
     first(): T | null;
 }
 
-export interface TransducerBuilder<BaseT, T> {
-    compose<U>(transducer: t.Transducer<T, U>): TransducerBuilder<BaseT, U>;
+export interface TransducerBuilder<TBase, T> {
+    compose<U>(transducer: t.Transducer<T, U>): TransducerBuilder<TBase, U>;
 
-    map<U>(f: (item: T) => U): TransducerBuilder<BaseT, U>;
-    filter(pred: (item: T) => boolean): TransducerBuilder<BaseT, T>;
-    remove(pred: (item: T) => boolean): TransducerBuilder<BaseT, T>;
-    keep<U>(f: (item: T) => U | null | void): TransducerBuilder<BaseT, U>;
-    mapcat<U>(f: (item: T) => U[]): TransducerBuilder<BaseT, U>;
-    dedupe(): TransducerBuilder<BaseT, T>;
-    take(n: number): TransducerBuilder<BaseT, T>;
-    takeWhile(pred: (item: T) => boolean): TransducerBuilder<BaseT, T>;
-    takeNth(n: number): TransducerBuilder<BaseT, T>;
-    drop(n: number): TransducerBuilder<BaseT, T>;
-    dropWhile(pred: (item: T) => boolean): TransducerBuilder<BaseT, T>;
-    partition(n: number): TransducerBuilder<BaseT, T[]>;
-    partitionBy(pred: (item: T) => boolean): TransducerBuilder<BaseT, T[]>;
-    interpose(separator: T): TransducerBuilder<BaseT, T>;
+    map<U>(f: (item: T) => U): TransducerBuilder<TBase, U>;
+    filter(pred: (item: T) => boolean): TransducerBuilder<TBase, T>;
+    remove(pred: (item: T) => boolean): TransducerBuilder<TBase, T>;
+    keep<U>(f: (item: T) => U | null | void): TransducerBuilder<TBase, U>;
+    mapcat<U>(f: (item: T) => U[]): TransducerBuilder<TBase, U>;
+    dedupe(): TransducerBuilder<TBase, T>;
+    take(n: number): TransducerBuilder<TBase, T>;
+    takeWhile(pred: (item: T) => boolean): TransducerBuilder<TBase, T>;
+    takeNth(n: number): TransducerBuilder<TBase, T>;
+    drop(n: number): TransducerBuilder<TBase, T>;
+    dropWhile(pred: (item: T) => boolean): TransducerBuilder<TBase, T>;
+    partition(n: number): TransducerBuilder<TBase, T[]>;
+    partitionBy(pred: (item: T) => boolean): TransducerBuilder<TBase, T[]>;
+    interpose(separator: T): TransducerBuilder<TBase, T>;
 
-    build(): t.Transducer<BaseT, T>;
+    build(): t.Transducer<TBase, T>;
 }
 
-export function chainFrom<T>(collection: T[] | Iterable<T>): TransformChain<T> {
+export function chainFrom<T>(collection: Iterable<T>): TransformChain<T> {
     return new TransducerChain<T, T>(collection);
 }
 
@@ -62,22 +62,22 @@ export function transducerBuilder<T>(): TransducerBuilder<T, T> {
     return new TransducerChain<T, T>([]);
 }
 
-type CombinedBuilder<BaseT, T> = TransformChain<T> &
-    TransducerBuilder<BaseT, T>;
+type CombinedBuilder<TBase, T> = TransformChain<T> &
+    TransducerBuilder<TBase, T>;
 
-class TransducerChain<BaseT, T> implements CombinedBuilder<BaseT, T> {
+class TransducerChain<TBase, T> implements CombinedBuilder<TBase, T> {
     private readonly transducers: Array<t.Transducer<any, any>> = [];
 
-    constructor(private readonly collection: BaseT[] | Iterable<BaseT>) {}
+    constructor(private readonly collection: Iterable<TBase>) {}
 
     public compose<U>(
         transducer: t.Transducer<T, U>,
-    ): CombinedBuilder<BaseT, U> {
+    ): CombinedBuilder<TBase, U> {
         this.transducers.push(transducer);
         return this as any;
     }
 
-    public build(): t.Transducer<BaseT, T> {
+    public build(): t.Transducer<TBase, T> {
         // Don't use comp() from transducers-js because it fails on 0 or 1
         // inputs.
         return (x: any) => {
@@ -89,40 +89,40 @@ class TransducerChain<BaseT, T> implements CombinedBuilder<BaseT, T> {
         };
     }
 
-    public reduce<ResultT>(
-        reducer: t.Reducer<ResultT, T>,
-        initialValue: ResultT,
-    ): ResultT;
-    public reduce<ResultT, CompleteResultT>(
-        transformer: t.CompletingTransformer<ResultT, CompleteResultT, T>,
-        initialValue?: ResultT,
-    ): CompleteResultT;
-    public reduce<ResultT, CompleteResultT>(
+    public reduce<TResult>(
+        reducer: t.Reducer<TResult, T>,
+        initialValue: TResult,
+    ): TResult;
+    public reduce<TResult, TCompleteResult>(
+        transformer: t.CompletingTransformer<TResult, TCompleteResult, T>,
+        initialValue?: TResult,
+    ): TCompleteResult;
+    public reduce<TResult, TCompleteResult>(
         transformer:
-            | t.Reducer<ResultT, T>
-            | t.CompletingTransformer<ResultT, CompleteResultT, T>,
-        initialValue: ResultT,
-    ): CompleteResultT {
+            | t.Reducer<TResult, T>
+            | t.CompletingTransformer<TResult, TCompleteResult, T>,
+        initialValue: TResult,
+    ): TCompleteResult {
         // Need to contort the type system a bit to get this overload.
         if (typeof transformer === "function") {
-            const result: ResultT = t.transduce<ResultT, BaseT, T>(
+            const result: TResult = t.transduce<TResult, TBase, T>(
                 this.build(),
                 transformer,
                 initialValue,
                 this.collection,
             );
-            // Safe because ResultT and CompleteResultT are the same in this
+            // Safe because TResult and TCompleteResult are the same in this
             // case.
             return result as any;
         } else {
             if (initialValue === undefined) {
-                return t.transduce<ResultT, CompleteResultT, BaseT, T>(
+                return t.transduce<TResult, TCompleteResult, TBase, T>(
                     this.build(),
                     transformer,
                     this.collection,
                 );
             } else {
-                return t.transduce<ResultT, CompleteResultT, BaseT, T>(
+                return t.transduce<TResult, TCompleteResult, TBase, T>(
                     this.build(),
                     transformer,
                     initialValue,
@@ -137,7 +137,10 @@ class TransducerChain<BaseT, T> implements CombinedBuilder<BaseT, T> {
     }
 
     public toIterator(): IterableIterator<T> {
-        return t.toIter(this.collection, this.build());
+        return new TransducerIterable(
+            this.build(),
+            this.collection[Symbol.iterator](),
+        );
     }
 
     public forEach(f: (item: T) => void): void {
@@ -148,59 +151,59 @@ class TransducerChain<BaseT, T> implements CombinedBuilder<BaseT, T> {
         return this.reduce(firstTransformer<T>());
     }
 
-    public map<U>(f: (item: T) => U): CombinedBuilder<BaseT, U> {
+    public map<U>(f: (item: T) => U): CombinedBuilder<TBase, U> {
         return this.compose(t.map(f));
     }
 
-    public filter(pred: (item: T) => boolean): CombinedBuilder<BaseT, T> {
+    public filter(pred: (item: T) => boolean): CombinedBuilder<TBase, T> {
         return this.compose(t.filter(pred));
     }
 
-    public remove(pred: (item: T) => boolean): CombinedBuilder<BaseT, T> {
+    public remove(pred: (item: T) => boolean): CombinedBuilder<TBase, T> {
         return this.compose(t.remove(pred));
     }
 
-    public keep<U>(f: (item: T) => U | null | void): CombinedBuilder<BaseT, U> {
+    public keep<U>(f: (item: T) => U | null | void): CombinedBuilder<TBase, U> {
         return this.compose(keep(f));
     }
 
-    public mapcat<U>(f: (item: T) => U[]): CombinedBuilder<BaseT, U> {
+    public mapcat<U>(f: (item: T) => U[]): CombinedBuilder<TBase, U> {
         return this.compose(t.mapcat(f));
     }
 
-    public dedupe(): CombinedBuilder<BaseT, T> {
+    public dedupe(): CombinedBuilder<TBase, T> {
         return this.compose(dedupe<T>());
     }
 
-    public take(n: number): CombinedBuilder<BaseT, T> {
+    public take(n: number): CombinedBuilder<TBase, T> {
         return this.compose(t.take<T>(n));
     }
 
-    public takeWhile(pred: (item: T) => boolean): CombinedBuilder<BaseT, T> {
+    public takeWhile(pred: (item: T) => boolean): CombinedBuilder<TBase, T> {
         return this.compose(t.takeWhile(pred));
     }
 
-    public takeNth(n: number): CombinedBuilder<BaseT, T> {
+    public takeNth(n: number): CombinedBuilder<TBase, T> {
         return this.compose(t.takeNth<T>(n));
     }
 
-    public drop(n: number): CombinedBuilder<BaseT, T> {
+    public drop(n: number): CombinedBuilder<TBase, T> {
         return this.compose(t.drop<T>(n));
     }
 
-    public dropWhile(pred: (item: T) => boolean): CombinedBuilder<BaseT, T> {
+    public dropWhile(pred: (item: T) => boolean): CombinedBuilder<TBase, T> {
         return this.compose(t.dropWhile(pred));
     }
 
-    public partition(n: number): CombinedBuilder<BaseT, T[]> {
+    public partition(n: number): CombinedBuilder<TBase, T[]> {
         return this.compose(t.partitionAll<T>(n));
     }
 
-    public partitionBy(pred: (item: T) => any): CombinedBuilder<BaseT, T[]> {
+    public partitionBy(pred: (item: T) => any): CombinedBuilder<TBase, T[]> {
         return this.compose(t.partitionBy(pred));
     }
 
-    public interpose(separator: T): CombinedBuilder<BaseT, T> {
+    public interpose(separator: T): CombinedBuilder<TBase, T> {
         return this.compose(interpose(separator));
     }
 }
@@ -355,4 +358,53 @@ const firstTransformerConstant: t.Transformer<any, any> = {
 
 function firstTransformer<T>(): t.Transformer<T | null, T> {
     return firstTransformerConstant;
+}
+
+class TransducerIterable<TInput, TOutput> implements IterableIterator<TOutput> {
+    private upcoming: Iterator<TOutput> = new ArrayIterator([]);
+
+    constructor(
+        private readonly xf: t.Transducer<TInput, TOutput>,
+        private readonly iterator: Iterator<TInput>,
+    ) {}
+
+    public [Symbol.iterator](): IterableIterator<TOutput> {
+        return this;
+    }
+
+    public next(): IteratorResult<TOutput> {
+        const backlogged = this.upcoming.next();
+        if (!backlogged.done) {
+            return backlogged;
+        } else {
+            const { done, value } = this.iterator.next();
+            if (done) {
+                return { done } as any;
+            } else {
+                const outValues = t.into([], this.xf, [value]);
+                this.upcoming = new ArrayIterator(outValues);
+                return this.next();
+            }
+        }
+    }
+}
+
+class ArrayIterator<T> implements IterableIterator<T> {
+    private i: number = 0;
+
+    constructor(private readonly array: T[]) {}
+
+    public [Symbol.iterator]() {
+        return this;
+    }
+
+    public next(): IteratorResult<T> {
+        const { i, array } = this;
+        if (i < array.length) {
+            this.i++;
+            return { done: false, value: array[i] };
+        } else {
+            return { done: true } as any;
+        }
+    }
 }
