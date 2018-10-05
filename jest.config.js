fba301cc03c02cbdd89eb23d67b753cef662a5f6
David Philipson <david.philipson@gmail.com>
2018-10-05 16:24:39 -0700

    Use auto-generated ts-jest config
    Created with `yarn ts-jest config:init`.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    transform: {
-        "^.+\\.tsx?$": "ts-jest",
-    },
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    preset: "ts-jest",
+    testEnvironment: "node",
 };
